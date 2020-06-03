d06c1490613376b7562033e3bd6d862924af6faa
Esther Ngamata <3stherngamata@gmail.com>
2020-06-03 13:34:47 +0000

    COMPX341-20 A4 VERSION 1

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -1,5 +1,5 @@
 
-var express = require('express');
+/** var express = require('express');
 var router = express.Router();
 var REQUEST = require('request');
 
@@ -8,7 +8,7 @@ var request = REQUEST.defaults( {
 });
 
 //var OPENWEATHERURL = "https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=imperial";
-var OPENWEATHERURL = "https://api.openweathermap.org/data/2.5/weather?q=Hamilton&appid=pk.eyJ1IjoiZXN0aGVybmdhbWF0YSIsImEiOiJja2F6Ym15Nm0waHVvMnptaTJhaDd2MDI0In0.4-XnqLJAgixPINBBkMcY3g&units=metric";
+var OPENWEATHERURL = "https://api.openweathermap.org/data/2.5/weather?appid=pk.eyJ1IjoiZXN0aGVybmdhbWF0YSIsImEiOiJja2F6Ym15Nm0waHVvMnptaTJhaDd2MDI0In0.4-XnqLJAgixPINBBkMcY3g&units=metric";
 
 exports.getWeather = function(req, res) {
 	var zip = req.query.zip;
@@ -70,7 +70,7 @@ exports.getWeather2 = function(req, res) {
 
 };
 router.get('/getWeather2', exports.getWeather2);
-*/
+
 exports.getWeather3 = function(req, res) {
 	var zip = req.query.zip;
 	if( (zip === null) || (typeof(zip) === 'undefined') ) {
@@ -101,4 +101,109 @@ exports.getWeather3 = function(req, res) {
 };
 router.get('/getWeather3', exports.getWeather3);
 
+exports.router = router; */
+
+var express = require('express');
+var router = express.Router();
+var REQUEST = require('request');
+
+var request = REQUEST.defaults( {
+    strictSSL: false
+});
+
+//var OPENWEATHERURL = "https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=imperial";
+var OPENWEATHERURL = "https://api.openweathermap.org/data/2.5/weather?appid=pk.eyJ1IjoiZXN0aGVybmdhbWF0YSIsImEiOiJja2F6Ym15Nm0waHVvMnptaTJhaDd2MDI0In0.4-XnqLJAgixPINBBkMcY3g&units=metric";
+
+exports.getWeather = function(req, res) {
+	var cityName = req.query.cityName;
+	if( (cityName === null) || (typeof(cityName) === 'undefined') ) {
+		return res.status(400).send('city name missing');
+	}
+
+	var aurl = OPENWEATHERURL + '&city=' + cityName + ',nz';
+
+	request({
+		method: 'GET',
+        url: aurl,
+  		json: true
+    }, function(err, resp, body) {
+    	if(err) {
+    		res.status(400).send('Failed to get the data');
+    		//console.error("Failed to send request to openweathermap.org", err);
+    	} else {
+    		if(body.cod === 200) {
+    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' F';
+    			var response = {city: body.name, weather: weath};
+    			return res.status(200).send(response);
+    		} else {
+                return res.status(400).send({msg:'Failed'});
+            }
+    	}
+    });
+
+};
+router.get('/getWeather', exports.getWeather);
+
+/*
+exports.getWeather2 = function(req, res) {
+	var zip = req.query.zip;
+	if( (zip === null) || (typeof(zip) === 'undefined') ) {
+		return res.status(400).send('zip missing');
+	}
+
+	var aurl = OPENWEATHERURL + '&zip=' + zip + ',us';
+
+	request({
+		method: 'GET',
+        url: aurl,
+  		json: true
+    }, function(err, resp, body) {
+    	if(err) {
+    		res.status(400).send('Failed to get the data');
+    		//console.error("Failed to send request to openweathermap.org", err);
+    	} else {
+    		if(body.cod === 200) {
+    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' F';
+    			var response = {city: body.name, weather: weath};
+    			return res.status(200).send(response);
+    		} else {
+                return res.status(400).send({msg:'Failed'});
+            }
+    	}
+    });
+
+};
+router.get('/getWeather2', exports.getWeather2);
+*/
+exports.getWeather3 = function(req, res) {
+	var cityName = req.query.cityName;
+	if( (cityName === null) || (typeof(cityName) === 'undefined') ) {
+		return res.status(400).send('city name missing');
+	}
+
+	var aurl = OPENWEATHERURL + '&city=' + cityName + ',nz';
+
+	request({
+		method: 'GET',
+        url: aurl,
+  		json: true
+    }, function(err, resp, body) {
+    	if(err) {
+    		res.status(400).send('Failed to get the data');
+    		//console.error("Failed to send request to openweathermap.org", err);
+    	} else {
+    		if(body.cod === 200) {
+    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' F';
+    			var response = {city: body.name, weather: weath};
+    			return res.status(200).send(response);
+    		} else {
+                return res.status(400).send({msg:'Failed'});
+            }
+    	}
+    });
+
+};
+router.get('/getWeather3', exports.getWeather3);
+
 exports.router = router;
+
--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,15 +1,57 @@
+/*
+//import React, { useState } from 'react';
+//import fetch from 'isomorphic-unfetch'
+//import ZipResponse from '../components/ZipResponse';
+//import Zip from '../components/Zip';
+
+//function AppContainer(props) {
+
+    //const [responseData, setResponseData] = useState('');
+
+    //const handleZipChange = async (zipValue) => {
+        //console.log(`--------- fetchData called zip:${zipValue}`)
+        //const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=imperial&zip=${zipValue},us`)
+        //const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?city=${cityName}&appid=pk.eyJ1IjoiZXN0aGVybmdhbWF0YSIsImEiOiJja2F6Ym15Nm0waHVvMnptaTJhaDd2MDI0In0.4-XnqLJAgixPINBBkMcY3g&units=metric,nz`)
+        //const json = await res.json()
+        //console.log(json);
+        //setResponseData(json);
+    //}
+
+    //const clearResponse = () => {
+        //setResponseData('');
+    //}
+
+    //return (
+        //<div>
+            //<div className="row mt-4">
+                //<div className="col-sm-4"></div>
+                //<Zip onZipChange={handleZipChange} clearResponse={clearResponse}/>
+                //<div className="col-sm-4"></div>
+            //</div>
+            //<div className="row mt-4">
+                //<div className="col-sm-2"></div>
+                //<ZipResponse responseData={responseData} clearResponse={clearResponse}/>
+                //<div className="col-sm-2"></div>
+            //</div>    
+        //</div>
+    //);
+//}
+  
+//export default AppContainer
+*/
 import React, { useState } from 'react';
 import fetch from 'isomorphic-unfetch'
-import ZipResponse from '../components/ZipResponse';
-import Zip from '../components/Zip';
+import CityNameResponse from '../components/CityNameResponse';
+import CityName from '../components/CityName';
 
 function AppContainer(props) {
 
     const [responseData, setResponseData] = useState('');
 
-    const handleZipChange = async (zipValue) => {
+    const handleCityNameChange = async (cityName) => {
         //console.log(`--------- fetchData called zip:${zipValue}`)
-        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=imperial&zip=${zipValue},us`)
+        //const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=imperial&zip=${zipValue},us`)
+        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?city=${cityName}&appid=pk.eyJ1IjoiZXN0aGVybmdhbWF0YSIsImEiOiJja2F6Ym15Nm0waHVvMnptaTJhaDd2MDI0In0.4-XnqLJAgixPINBBkMcY3g&units=metric,nz`)
         const json = await res.json()
         //console.log(json);
         setResponseData(json);
@@ -23,12 +65,12 @@ function AppContainer(props) {
         <div>
             <div className="row mt-4">
                 <div className="col-sm-4"></div>
-                <Zip onZipChange={handleZipChange} clearResponse={clearResponse}/>
+                <CityName onCityNameChange={handleCityNameChange} clearResponse={clearResponse}/>
                 <div className="col-sm-4"></div>
             </div>
             <div className="row mt-4">
                 <div className="col-sm-2"></div>
-                <ZipResponse responseData={responseData} clearResponse={clearResponse}/>
+                <CityNameResponse responseData={responseData} clearResponse={clearResponse}/>
                 <div className="col-sm-2"></div>
             </div>    
         </div>
